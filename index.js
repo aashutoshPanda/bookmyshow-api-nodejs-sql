1fce9ace0bc593a0a85b464573b9f157d1c6b526
ashutoshPanda <iamashutoshpanda@gmail.com>
2023-05-24 14:30:22 +0530

    REM : redundant middlewares

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import express from "express";
-import logger from "morgan";
 import dotenv from "dotenv";
-import connectDB from "./src/helpers/mongoose.js";
 import routes from "./src/routes/index.js";
-import rateLimit from "express-rate-limit";
-import loggerMiddleware from "./src/middlewares/logger.js";
 
 // Make all variables from our .env file available in our process
 dotenv.config({ path: ".env.example" });
@@ -12,22 +8,8 @@ dotenv.config({ path: ".env.example" });
 // Init express server
 const app = express();
 
-// Connect to MongoDB.
-await connectDB();
-
 //rate limit
 
-const limiter = rateLimit({
-  windowMs: 10 * 1000,
-  max: 3,
-  message: "Too many requests from this IP, please try again later.",
-});
-if (process.env.NODE_ENV !== "TEST") {
-  app.use(limiter);
-}
-app.use(loggerMiddleware());
-// Middlewares & configs setup
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
